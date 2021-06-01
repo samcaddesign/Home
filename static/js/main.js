43bc0b3dabc782df98dfe15f3edacaae0014f915
samcaddesign <munigopalakrishna.med@gmail.com>
2021-06-01 13:40:42 +0530

    aos-data_css_js_added

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,23 @@
-
 var swiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
-    slidesPerView: 'auto',
-    spaceBetween: 10,
-    centeredSlides: true,
-    grabCursor: true,
-    loop: true,
-    coverflowEffect: {
-        rotate: 40,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+  effect: 'coverflow',
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  centeredSlides: true,
+  grabCursor: true,
+  loop: true,
+  coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+  },
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
 });
